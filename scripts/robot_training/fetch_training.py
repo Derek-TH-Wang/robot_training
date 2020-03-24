84cc450df15d34318af1d40e63dd260ea522a6e8
Derek-TH-Wang <416338223@qq.com>
2020-03-24 10:56:26 +0800

    init update

---
+++ scripts/robot_training/fetch_training.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import os
+import sys
+import rospy
+import rospkg
+from robot_training.util.utility import  EnvRegister
+from robot_training.rl_algorithms.DQNRobotSolver import DQNRobotSolver
+
+if __name__ == '__main__':
+    rospy.init_node('fetch_n1try_algorithm', anonymous=True, log_level=rospy.WARN)
+
+    rospy.logwarn("read parm from yaml")
+    task_env_name = rospy.get_param('/fetch/task_and_robot_environment_name')
+    rospy.logwarn("task_and_robot_environment_name ==>" + str(task_env_name))
+    n_observations = rospy.get_param('/fetch/n_observations')
+    n_actions = rospy.get_param('/fetch/n_actions')
+    n_episodes_training = rospy.get_param('/fetch/episodes_training')
+    n_episodes_running = rospy.get_param('/fetch/episodes_running')
+    n_win_ticks = rospy.get_param('/fetch/n_win_ticks')
+    min_episodes = rospy.get_param('/fetch/min_episodes')
+    max_env_steps = None
+    gamma = rospy.get_param('/fetch/gamma')
+    epsilon = rospy.get_param('/fetch/epsilon')
+    epsilon_min = rospy.get_param('/fetch/epsilon_min')
+    epsilon_log_decay = rospy.get_param('/fetch/epsilon_decay')
+    alpha = rospy.get_param('/fetch/alpha')
+    alpha_decay = rospy.get_param('/fetch/alpha_decay')
+    batch_size = rospy.get_param('/fetch/batch_size')
+    monitor = rospy.get_param('/fetch/monitor')
+    quiet = rospy.get_param('/fetch/quiet')
+    rospackage_name = "robot_training"
+    model_name = "fetch_dqn"
+
+    rospy.logwarn("env_register")
+    env_object = EnvRegister(task_env_name)
+
+    rospy.logwarn("Starting Learning")
+    agent = DQNRobotSolver(env_object,
+                           task_env_name,
+                           n_observations,
+                           n_actions,
+                           n_win_ticks,
+                           min_episodes,
+                           max_env_steps,
+                           gamma,
+                           epsilon,
+                           epsilon_min,
+                           epsilon_log_decay,
+                           alpha,
+                           alpha_decay,
+                           batch_size,
+                           monitor,
+                           quiet)
+    agent.run(num_episodes=n_episodes_training, do_train=True)
+
+    rospy.logwarn("training over")
+    rospack = rospkg.RosPack()
+    pkg_path = rospack.get_path(rospackage_name)
+    outdir = pkg_path + '/models'
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+        rospy.logfatal("Created folder="+str(outdir))
+
+    agent.save(model_name, outdir)
+    agent.load(model_name, outdir)
+
+    agent.run(num_episodes=n_episodes_running, do_train=False)
---
+++ scripts/robot_training/util/utility.py
@@ -0,0 +1,86 @@
+import gym
+from gym import envs
+from gym.envs.registration import register
+import rospy
+import rosparam
+import rospkg
+import os
+import roslaunch
+import git
+import sys
+import subprocess
+
+
+def EnvRegister(task_env, max_episode_steps=10000):
+    register(
+        id=task_env,
+        entry_point='task_envs.fetch.fetch_test_task:FetchTestEnv',
+        max_episode_steps=max_episode_steps,
+    )
+    from task_envs.fetch import fetch_test_task
+
+    # We check that it was really registered
+    all_envs = envs.registry.all()
+    env_ids = [env_spec.id for env_spec in all_envs]
+    # print("REGISTERED GYM ENVS===>"+str(env_ids))
+    assert (task_env in env_ids), "The Task_Robot_ENV given is not Registered ==>" +  str(task_env)
+    
+    rospy.logwarn("Register of Task Env went OK, lets make the env..."+str(task_env))
+    env = gym.make(task_env)
+    rospy.logwarn("gym make finish")
+    return env
+
+def LoadYamlFileParamsTest(rospackage_name, rel_path_from_package_to_file, yaml_file_name):
+
+    rospack = rospkg.RosPack()
+    pkg_path = rospack.get_path(rospackage_name)
+    config_dir = os.path.join(pkg_path, rel_path_from_package_to_file) 
+    path_config_file = os.path.join(config_dir, yaml_file_name)
+    
+    paramlist=rosparam.load_file(path_config_file)
+    
+    for params, ns in paramlist:
+        rosparam.upload_params(ns,params)
+
+class ROSLauncher(object):
+    def __init__(self, rospackage_name, launch_file_name, ros_ws_abspath="/home/derek/openai_ws"):
+
+        self._rospackage_name = rospackage_name
+        self._launch_file_name = launch_file_name
+
+        self.rospack = rospkg.RosPack()
+        pkg_path = self.rospack.get_path(rospackage_name)
+        if pkg_path:
+            # If the package was found then we launch
+            rospy.loginfo(
+                ">>>>>>>>>>Package found in workspace-->"+str(pkg_path))
+            launch_dir = os.path.join(pkg_path, "launch")
+            path_launch_file_name = os.path.join(launch_dir, launch_file_name)
+
+            rospy.logwarn("path_launch_file_name=="+str(path_launch_file_name))
+
+            source_env_command = "source "+ros_ws_abspath+"/devel/setup.bash;"
+            roslaunch_command = "roslaunch  {0} {1}".format(rospackage_name, launch_file_name)
+            command = source_env_command+roslaunch_command
+            rospy.logwarn("Launching command="+str(command))
+
+            p = subprocess.Popen(command, shell=True)
+
+            state = p.poll()
+            if state is None:
+                rospy.loginfo("process is running fine")
+            elif state < 0:
+                rospy.loginfo("Process terminated with error")
+            elif state > 0:
+                rospy.loginfo("Process terminated without error")
+            """
+            self.uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+            roslaunch.configure_logging(self.uuid)
+            self.launch = roslaunch.parent.ROSLaunchParent(
+                self.uuid, [path_launch_file_name])
+            self.launch.start()
+            """
+            rospy.loginfo(">>>>>>>>>STARTED Roslaunch-->" +
+                            str(self._launch_file_name))
+        else:
+            rospy.logfatal("No package found")
